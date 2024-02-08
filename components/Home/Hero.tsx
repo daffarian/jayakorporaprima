3836b35c09eec3f296e319917e1f9f239ee1aaf4
daffarian <mdaffarian123@gmail.com>
2024-02-08 12:38:04 +0700

    change hero video

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -12,8 +12,8 @@ export default function Hero() {
         loop
         src="/video/city-light.mp4"
       ></video>
-      <div className="absolute inset-0 bg-black/10 z-[1] flex flex-col items-center justify-center text-white p-5">
-        <h1 className="font-medium translate-y-16 text-5xl">Security & Access System Solution</h1>
+      <div className="absolute inset-0 bg-black/30 z-[1] flex flex-col items-center justify-center text-white p-5">
+        <h1 className="font-medium translate-y-28 lg:text-7xl text-5xl">Security & Access System Solution</h1>
       </div>
     </div>
   );
