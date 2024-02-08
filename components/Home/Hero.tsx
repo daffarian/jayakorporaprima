80b042ca90d535b5a9eaca3838a03bae0d17b034
daffarian <mdaffarian123@gmail.com>
2024-02-08 21:31:04 +0700

    bag

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -11,9 +11,9 @@ export default function Hero() {
           loop
           src="/video/city-light.mp4"
         ></video>
-        <div className="absolute inset-0 bg-black/30 z-[1] flex flex-col lg:items-center  items-start justify-end gap-4 p-5">
+        <div className="absolute inset-0 bg-black/50 z-[1] flex flex-col lg:items-center  items-start justify-end gap-4 p-5">
           <div className="flex flex-col  lg:max-w-2xl lg:justify-center lg:mb-20 gap-6 container mx-auto">
-            <h1 className="font-medium  lg:text-7xl text-6xl text-transparent bg-clip-text bg-gradient-to-r from-white to-primary">
+            <h1 className="font-medium  lg:text-7xl text-5xl text-white">
               Security & Access System Solution
             </h1>
             <div>
