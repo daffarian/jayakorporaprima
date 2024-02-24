f7201448b387f1dac492dc51da7d3c9a6618da30
daffarian <mdaffarian123@gmail.com>
2024-02-25 02:59:10 +0700

    add parking again

--- components/home/ProductServices.tsx
+++ components/home/ProductServices.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Image from 'next/image';
-import GoTo from '../GoTo';
-
+import { Icons } from '../Icons';
 export default function ProductServices() {
   return (
     <section>
@@ -21,24 +20,61 @@ export default function ProductServices() {
               latest technology, we present integrated solutions for protect
               your assets and ensure maximum security.
             </p>
-            <GoTo link="product-&-service">More</GoTo>
+            {/* <GoTo link="product-&-service">More</GoTo> */}
           </div>
         </div>
       </div>
       {/* Products And Services */}
-      <div className="">
+      <div className="mt-20">
+        {/* Business Consultant */}
         <div className="flex flex-col">
-          <h3 className="border-l-2 border-primary mx-auto inline-flex text-center px-4">Business Consultant</h3>
-          <Image
-            src={'/images/decoration/handshake.png'}
-            width={500}
-            height={100}
-            alt="Business Consultant"
-          />
-          <p className='px-4 text-center'>
-            Consult on your business and project problems and find comprehensive
-            solutions with us
+          <h3 className="border-l-2 border-primary mx-auto inline-flex text-center px-4">
+            Business Consultant
+          </h3>
+          <div className="grid grid-cols-2 mt-10">
+            <Image
+              src={'/images/decoration/handshake.png'}
+              width={200}
+              height={100}
+              alt="Business Consultant"
+            />
+            <p className="text-center bg-secondary text-neutral-200 rounded-l-md p-4">
+              Consult on your business and project problems and find
+              comprehensive solutions with us
+            </p>
+          </div>
+        </div>
+        {/* Parking Solution */}
+        <div className="flex flex-col mt-20">
+          <h3 className="border-l-2 border-primary mx-auto inline-flex text-center px-4">
+            Parking Solution
+          </h3>
+          <p className="text-center px-4 mt-10">
+            Latest tools and technology for effective, efficient and safe
+            parking management.
           </p>
+          {/* Sub Parking */}
+          <div className="px-4 mt-5">
+            <div className="p-5 rounded-xl shadow-xl">
+              <Icons.service className='w-20'/>
+              <h4 className="text-primary mt-5">Parking Management Service</h4>
+              <p className='mt-5'>
+                Apart from providing parking solutions, we have an integrated
+                parking system network and manage parking in various locations
+              </p>
+            </div>
+            <div className="p-5 rounded-xl shadow-xl mt-10">
+              <Icons.tech className='w-20'/>
+              <h4 className="text-primary mt-5">
+                The Best Parking Equipment & Supplies
+              </h4>
+              <p className='mt-5'>
+                Supported by the best equipment made in Europe & Asia, this
+                makes us a company that is ready to provide various kinds of
+                parking equipment & equipment needs
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
