30a33e7c69040565817a8ddec8e4d86350db6010
daffarian <mdaffarian123@gmail.com>
2024-02-25 00:34:37 +0700

    muted

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,7 +4,7 @@ import Button from './Button';
 export default function Hero() {
   return (
     <section className="hero-section relative w-full h-svh">
-      <video className="absolute h-full w-full" autoPlay loop>
+      <video className="absolute h-full w-full" autoPlay muted loop>
         <source src="/video/hero.mp4" type="video/mp4" />
       </video>
       <div className="absolute inset-0 z-[1] flex flex-col lg:items-center  items-start justify-end gap-4 pb-8">
