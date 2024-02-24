45034e3b9bfa91e4dd60c71d44c7f4888549456d
daffarian <mdaffarian123@gmail.com>
2024-02-24 18:10:27 +0700

    cussss

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,11 +1,8 @@
-'use client'
+'use client';
 import Button from './Button';
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function Hero() {
-  const viewport = window
-  const [viewPort, setViewPort] = useState();
-
   return (
     <section className="hero-section relative w-full h-svh">
       <video
@@ -14,7 +11,7 @@ export default function Hero() {
         muted={false}
         loop
         src="/video/hero.mp4"
-      ></video>g
+      ></video>
       <div className="absolute inset-0 z-[1] flex flex-col lg:items-center  items-start justify-end gap-4 pb-8">
         <div className="flex flex-col  lg:max-w-2xl lg:justify-center lg:mb-20 gap-6 container mx-auto">
           {/* <h1 className="lg:text-7xl text-5xl text-white">
