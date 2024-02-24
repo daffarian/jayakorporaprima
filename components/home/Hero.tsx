4408043d6cac304cfcf87ba3ca07116f30085a6b
daffarian <mdaffarian123@gmail.com>
2024-02-24 18:02:00 +0700

    change bg hero

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
         muted={false}
         loop
         src="/video/hero.mp4"
-      ></video>
+      ></video>g
       <div className="absolute inset-0 z-[1] flex flex-col lg:items-center  items-start justify-end gap-4 pb-8">
         <div className="flex flex-col  lg:max-w-2xl lg:justify-center lg:mb-20 gap-6 container mx-auto">
           {/* <h1 className="lg:text-7xl text-5xl text-white">
