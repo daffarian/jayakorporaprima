d829d181af463b6dcbdd4307a5bfea6fa3f8a395
daffarian <mdaffarian123@gmail.com>
2024-02-24 17:51:42 +0700

    change video

--- components/home/Button.tsx
+++ components/home/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
   return (
-    <button className="text-white px-2 py-1 border-white border-[1px] hover:text-black  hover:bg-white bg-transparent">
+    <button className="text-white px-2 py-1 bg-primary shadow-xl border-[1px]">
       <a href="https://wa.me/6285771679611">Contact Us</a>
     </button>
   );
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,20 +1,25 @@
+'use client'
 import Button from './Button';
-import React from 'react';
+import {useState} from 'react';
+
 export default function Hero() {
+  const viewport = window
+  const [viewPort, setViewPort] = useState();
+
   return (
     <section className="hero-section relative w-full h-svh">
       <video
-        className="absolute h-full w-full object-cover"
+        className="absolute h-full w-full"
         autoPlay
-        muted
+        muted={false}
         loop
-        src="/video/city-light.mp4"
+        src="/video/hero.mp4"
       ></video>
-      <div className="absolute inset-0 bg-black/30 z-[1] flex flex-col lg:items-center  items-start justify-end gap-4 pb-8">
+      <div className="absolute inset-0 z-[1] flex flex-col lg:items-center  items-start justify-end gap-4 pb-8">
         <div className="flex flex-col  lg:max-w-2xl lg:justify-center lg:mb-20 gap-6 container mx-auto">
-          <h1 className="lg:text-7xl text-5xl text-white">
+          {/* <h1 className="lg:text-7xl text-5xl text-white">
             Access Control System
-          </h1>
+          </h1> */}
           <div>
             <Button />
           </div>
