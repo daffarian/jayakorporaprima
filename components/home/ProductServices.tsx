0cfa64859b732b32f2171dd5eeb16ec0854b5582
daffarian <mdaffarian123@gmail.com>
2024-02-28 06:11:43 +0700

    add man

--- components/home/ProductServices.tsx
+++ components/home/ProductServices.tsx
@@ -12,8 +12,8 @@ export default function ProductServices() {
       >
         <div className="bg-gradient-to-t lg:container gap-4 from-white via-white py-10 flex flex-col md:flex-row">
           <div className="container min-w-[20rem]">
-            <h2 className="my-10">Products & Services</h2>
-            <p className="max-w-md">
+            <h2 className="mt-10">Products & Services</h2>
+            <p className="max-w-md mt-5">
               We provide reliable security and access control system solutions
               for your business needs. With an innovative approach and the
               latest technology, we present integrated solutions for protect
@@ -30,7 +30,7 @@ export default function ProductServices() {
           <h3 className="border-l-2 border-primary mx-auto inline-flex text-center px-4">
             Business Consultant
           </h3>
-          <div className="mt-10 grid grid-cols-1 lg:grid-cols-2 place-items-center lg:px-4">
+          <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 place-items-center lg:px-4">
             <p className="text-center px-4">
               Consult on your business and project problems and find
               comprehensive solutions with us
@@ -49,13 +49,13 @@ export default function ProductServices() {
           <h3 className="border-l-2 border-primary mx-auto inline-flex text-center px-4">
             Parking Solution
           </h3>
-          <p className="text-center px-4 mt-10">
+          <p className="text-center px-4 mt-5">
             Latest tools and technology for effective, efficient and safe
             parking management.
           </p>
           {/* Sub Parking */}
-          <div className="px-4 mt-5 grid grid-cols-1 gap-5">
-            <div className="p-5 rounded-xl shadow-xl border-[1px] bg-cyan-50 border-neutral-200">
+          <div className="px-4 mt-10 grid grid-cols-1 gap-5">
+            <div className="p-5 rounded-xl shadow-xl border-[1px] bg-blue-50 border-neutral-200">
               {/* <Icons.service className="w-20" /> */}
               <h4 className="text-primary mt-5">Parking Management Service</h4>
               <p className="mt-5">
@@ -70,7 +70,7 @@ export default function ProductServices() {
                 className="mt-10 rounded-md"
               />
             </div>
-            <div className="p-5 rounded-xl shadow-xl mt-10g border-[1px] bg-cyan-50 border-neutral-200">
+            <div className="p-5 rounded-xl shadow-xl mt-10g border-[1px] bg-blue-50 border-neutral-200">
               {/* <Icons.tech className="w-20" /> */}
               <h4 className="text-primary mt-5">
                 The Best Parking Equipment & Supplies
@@ -95,7 +95,7 @@ export default function ProductServices() {
           <h3 className="border-l-2 border-primary mx-auto inline-flex text-center px-4">
             Security System
           </h3>
-          <div className="mt-10 grid grid-cols-1 lg:grid-cols-2 place-items-center lg:px-4">
+          <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 place-items-center lg:px-4">
             <p className="text-center px-4">
               We have a wide range of security tools & equipment to ensure
               maximum security
@@ -114,7 +114,7 @@ export default function ProductServices() {
           <h3 className="border-l-2 border-primary mx-auto inline-flex text-center px-4">
             Access Control System
           </h3>
-          <div className="mt-10 grid grid-cols-1 lg:grid-cols-2 place-items-center lg:px-4">
+          <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 place-items-center lg:px-4">
             <p className="text-center px-4">
               Latest & advanced tools & equipment for your access control needs
             </p>
