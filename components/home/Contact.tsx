e302f178f9c4390503cbc16a3dd24b9b5c39bc82
daffarian <mdaffarian123@gmail.com>
2024-02-28 12:01:32 +0700

    no dodr

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
       <div className="rounded-[2rem] border-[1px] border-slate-200 px-4 py-10">
         <h2 className="">Contact Us</h2>
         <div className="grid grid-cols-1 mt-10 lg:grid-cols-2 gap-10">
-          <address className="grid grid-cols-1 gap-5 order-last lg:order-1 not-italic">
+          <address className="grid grid-cols-1 gap-5 lg:order-1 not-italic">
             <div>
               <h3 className="text-xl">Operational hour </h3>
               <ul className="list-disc pl-5 text-neutral-800 mt-2 font-medium">
@@ -49,7 +49,7 @@ export default function Contact() {
               </a>
             </div>
           </address>
-          <div className="order-1">
+          <div className="">
             <iframe
               className="aspect-square w-full border-blue-400 border-[1px] rounded-sm"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1179.357631479446!2d106.85408488279023!3d-6.1481002459953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f59a5e80e0df%3A0x5df08484cfaee4d5!2sThe%20Mansion%20Bougenville%20Fontana!5e0!3m2!1sid!2sid!4v1707643203732!5m2!1sid!2sid"
